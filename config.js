7650666998cc4f0868c7b9bb8d5dfccf647737e8
SpeedxzpDev <speedkkzgui@gmail.com>
2025-12-09 20:28:23 -0300

    Eu to perdido

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-const prefixo = "/"
+require("dotenv").config();
+
+const prefixo = process.env.PREFIXO
 
 const botName = "Yuki"
 
 const version = "3.2.0"
 
-const numberOwner = [
-  "188123996786820@lid",
-  "221856653123760@lid"
-];
+const numberOwner = "188123996786820@lid"
 
 const numberBot = "11051571658890@lid"
 
