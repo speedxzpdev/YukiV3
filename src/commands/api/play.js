48aa224fc58596f3e6d88f67c479a5e6f7397821
SpeedxzpDev <speedkkzgui@gmail.com>
2025-12-30 01:05:12 -0300

    Resolução e melhoria do /play

--- src/commands/api/play.js
+++ src/commands/api/play.js
@@ -1,6 +1,5 @@
-const { exec, execSync} = require("child_process");
-const fs = require("fs");
-
+const axios = require("axios");
+require("dotenv").config();
 
 module.exports = {
   name: "play",
@@ -9,42 +8,55 @@ module.exports = {
     const texto = args.slice(0).join(" ")?.trim();
       try {
     if (!texto) {
-      await reply("FILHO DA PUTA BURRO DO KRL USA ESSA PORRA DIREITO SEU BOSTA")
+      await sock.sendMessage(from, {text: "FILHO DA PUTA BURRO DO KRL USA ESSA PORRA DIREITO SEU BOSTA"}, {quoted: msg});
       return
     }
-
-    const pesquisa = encodeURIComponent(texto)
-    await sock.sendMessage(from, {text: espera_pronta}, {quoted: msg});
-
-    const infoJson = execSync(`yt-dlp -j 'ytsearch1:${texto}'`)
-    const info = JSON.parse(infoJson)
-    const audio = execSync(`yt-dlp -x --audio-format mp3 "https://www.youtube.com/watch?v=${info.id}" -o "../assets/temp/${info.id}.mp3"`)
-
-
-    const legenda = `*Yuki Donwloads*
-> informações do criador:
-    *autor*: ${info.uploader}
-    *url do canal*: ${info.uploader_url}
-> informações do video:
-    *titulo*: ${info.title}
-    *curtidas*: ${info.like_count}
-    *vizualizações*: ${info.view_count}
-    *Data de upload*: ${info.upload_date}
-    *Duração*: ${info.duration}segundos
-    *comentarios*: ${info.comment_count}
-    *descrição*: ${info.description}
-    `
     
-    await sock.sendMessage(from, { image: { url: info.thumbnail }, caption: legenda}, {quoted: msg})
-
-
-    await sock.sendMessage(from, { audio: { url: `../assets/temp/${info.id}.mp3` }, mimetype: "audio/mpeg"}, { quoted: msg });
-
-    fs.unlinkSync(`../assets/temp/${info.id}.mp3`)
-
-
-
-
+    const frases = [`Buscando por ${texto}... Acalma o cu, arrombado!`, `Estou preparando um bolo enquanto busco por ${texto}...`, `${texto}...? Parece interessante, vou baixar...`];
+    
+    const randomFrase = frases[Math.floor(Math.random() * frases.length)];
+    
+    const msgEspera = await sock.sendMessage(from, {text: randomFrase}, {quoted: msg});
+    
+    //Busca pelo os metadados
+    const response = await axios.get(`https://zero-two-apis.com.br/api/ytsrc/videos?q=${encodeURIComponent(texto)}&apikey=${process.env.ZEROTWO_APIKEY}`);
+    
+    //pega o primeiro resultado
+    const data = response.data.resultado[0];
+    
+    const url = data.url
+    
+    const thumbnail = data.thumbnail
+    
+    const info = `*Yuki Youtube!*
+> Autor do video:
+*Nome:* ${data.author.name}
+*Url:* ${data.author.url}
+> Video:
+*Título:* ${data.title}
+*Views:* ${data.views}
+*Postado:* ${data.ago}
+*Duração:* ${data.timestamp}
+*Descrição:* ${data.description}
+`
+    
+    await sock.sendMessage(from, {image: {url: thumbnail}, caption: info});
+    
+    
+    await sock.sendMessage(from, {text: "Buscando áudio...", edit: msgEspera.key});
+    
+    await sock.sendPresenceUpdate("recording", from);
+    
+    //audio
+    const audio = `https://zero-two-apis.com.br/api/dl/ytaudio?url=${encodeURIComponent(url)}&apikey=${process.env.ZEROTWO_APIKEY}`;
+    
+    await sock.sendMessage(from, {audio: {url: audio}, mimetype: "audio/mpeg", ptt: false}, {quoted: msg});
+    
+    await sock.sendMessage(from, {text: "Prontinho! Aqui seu áudio", edit: msgEspera.key});
+    
+    await sock.sendPresenceUpdate("paused", from);
+    
+    
   }
   catch(err) {
     await sock.sendMessage(from, {text: erros_prontos}, {quoted: msg});
